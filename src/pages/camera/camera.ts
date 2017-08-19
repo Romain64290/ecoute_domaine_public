3e14169b93daf2989d28aa33cdb41210f26d9d15
Romain TALDU <r.taldu@agglo-pau.fr>
2017-08-19 16:43:29 +0200

    genere un unique id pour identifier la photo

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -9,15 +9,28 @@ import { SignalementPage } from '../signalement/signalement';
 export class CameraPage {
 
   id:number;
+  uidPhoto:string;
 
   constructor(public navCtrl: NavController,private navParams:NavParams ) {
     this.id= navParams.get('id');
+    this.uidPhoto= this.guid();
 
   }
 
   goSignalementPage(): void {
     this.navCtrl.push(SignalementPage, {id: this.id});
 
+  }
+
+  // genere un id unique 
+  guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
   }
 
 }
